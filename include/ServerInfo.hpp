9b93f301d8cea442ba1820d0f9dba8453ef936dd
objectio <objection@kakao.com>
2023-02-05 01:27:18 +0900

    [CHORE] Rename Server.hpp to ServerInfo.hpp

--- include/ServerInfo.hpp
+++ include/ServerInfo.hpp
@@ -1,23 +1,27 @@
-#ifndef SERVER_HPP
-#define SERVER_HPP
+#ifndef SERVERINFO_HPP
+#define SERVERINFO_HPP
 
 #include <iostream>
 #include <vector>
+#include "LocationInfo.hpp"
 
-class Server
+class ServerInfo
 {
   private:
 	std::vector<std::string>	server_name;
 	unsigned int				port;
 	std::string					root;
-	std::vector<Location>		location;
+	std::vector<LocationInfo>	location;
 	std::vector<std::string>	allow_methods;
 	std::vector<std::string>	cgi;
 	std::vector<std::string>	cgi_path;
-};
+	//ErrorPage					error_page;
+	unsigned int				client_max_body_size;
+	bool						auto_index;
 
-class Location {
-	std::string	fastcgi;
+  public:
+	/* SETTER */
+	
 };
 
 #endif
